a8d2d1e38024f9ceb9037c9300271342d3abe0ff
Sergey Babkin <sergeybabkin1@gmail.com>
2022-11-04 17:59:10 +0300

    feat: sprint 16 (add-docker) rev.1

---
+++ gateway/src/main/java/ru/practicum/shareit/exception/ItemTransactionException.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.exception;
+
+public class ItemTransactionException extends RuntimeException {
+
+    public ItemTransactionException(String message) {
+
+        super(message);
+    }
+}
