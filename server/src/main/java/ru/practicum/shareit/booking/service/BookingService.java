c4f73b75773dfe835445100651802def56745722
Sergey Babkin <sergeybabkin1@gmail.com>
2022-11-06 14:07:16 +0300

    feat: sprint 16 (add-docker) rev.5

---
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.booking.service;
+
+import ru.practicum.shareit.booking.model.BookingDto;
+import ru.practicum.shareit.booking.model.BookingDtoSimple;
+
+import java.util.Collection;
+
+public interface BookingService {
+    BookingDto save(BookingDtoSimple bookingDtoSimple, Long userId);
+
+    BookingDto update(Long bookingId, BookingDto bookingDto);
+
+    void deleteById(Long bookingId);
+
+    BookingDto findById(Long bookingId, Long userId);
+
+    Collection<BookingDto> findAll(Long userId, String state, int from, int size);
+
+    Collection<BookingDto> findAllByItemOwnerId(Long userId, String state, int from, int size);
+
+    BookingDto approve(Long userId, Long bookingId, Boolean approved);
+}
